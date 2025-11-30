e8bbfe84b6bb0cd4f9e0c87971fa63015504245d
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-11-29 20:47:34 -0800

    shader test env

--- include/general/app_util.hpp
+++ include/general/app_util.hpp
@@ -3,4 +3,3 @@
 #include <GLFW/glfw3.h>
 
 GLFWwindow* create_window();
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
--- src/general/app_util.cpp
+++ src/general/app_util.cpp
@@ -4,6 +4,14 @@
 const unsigned int SCR_WIDTH  = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
 GLFWwindow* create_window() {
     // glfw: initialize and configure
     // ------------------------------
@@ -41,13 +49,4 @@ GLFWwindow* create_window() {
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
     return window;
-}
-
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
